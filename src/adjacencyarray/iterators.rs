63ea2059b5280060a7d1af7cf4af8001923aaba8
Sebastian Schmidt <isibboi@gmail.com>
2019-08-29 08:15:54 +0300

    Fix build warnings

--- src/adjacencyarray/iterators.rs
+++ src/adjacencyarray/iterators.rs
@@ -1,7 +1,6 @@
 //! Iterator types for the `AdjacencyArray`.
 
 use crate::{EdgeId, IdType, NodeId};
-use crate::graph::EdgeRef;
 
 /// An iterator over the nodes of an `AdjacencyArray`.
 pub type AdjacencyArrayNodeIdIterator =
@@ -9,18 +8,3 @@ pub type AdjacencyArrayNodeIdIterator =
 /// An iterator over the edges of an `AdjacencyArray`.
 pub type AdjacencyArrayEdgeIdIterator =
     std::iter::Map<std::ops::Range<IdType>, fn(IdType) -> EdgeId>;
-
-/// An iterator over the out-edges of a node.
-pub struct OutEdgeIterator<'a, E> {
-    start: NodeId,
-    edge_ends: &'a [NodeId],
-    edge_data: &'a [E],
-}
-
-impl<'a, E> Iterator for OutEdgeIterator<'a, E> {
-    type Item = EdgeId;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        unimplemented!()
-    }
-}
